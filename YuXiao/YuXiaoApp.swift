da1b8cabee653c4f2a2fc01c0ac3795c27f9f043
苏苏炎 <susuyan@163.com>
2024-08-15 09:27:54 +0800

    Initial Commit

---
+++ YuXiao/YuXiaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  YuXiaoApp.swift
+//  YuXiao
+//
+//  Created by 苏苏炎 on 2024/8/15.
+//
+
+import SwiftUI
+
+@main
+struct YuXiaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
